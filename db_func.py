3697cfd52d1ad916f386012fa57653d3bb6f673e
Lori Bard <lori@bardfamily.org>
2016-09-04 22:12:44 +0000

    worked on bootstrap, css and html

--- db_func.py
+++
@@ -1,7 +0,0 @@
-from model import db, connect_to_db, make_test_data, User, Category, UserCategory
-
-def get_category(user_category_id):
-    category =  UserCategory.query.filter_by(
-             user_category_id=user_category_id).first()
-
-    return category.category_id
